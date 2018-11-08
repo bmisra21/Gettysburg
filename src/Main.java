9e8645e235dbe48cf07df6f5087e715b5a1a569c
bmisra21 <42977074+bmisra21@users.noreply.github.com>
2018-11-08 13:37:24 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +12,45 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws FileNotFoundException 
 	{
 		Scanner file = new Scanner(new File("GettysburgAddress.txt"));
         // assume filename stores the name of the file 
 		List<String> words = new ArrayList<String>();
 		while (file.hasNext()) 
 		{
-			words = file.next();
+			String thing;
+			thing = file.next();
+			words.add(thing);
 		} 
-
+		
+		for (int i=0; i<words.size(); i++)
+		{
+			for (int h =0; h<words.get(i).length();h++) 
+			{
+				if (words.get(i).contains(",") || words.get(i).contains(".") || words.get(i).contains("-"))
+				{
+					words.add(i, words.get(i).substring(h));
+				}
+					
+			}
+			
+		} 
+		
+		for (int i=0; i<words.size(); i++)
+		{
+			System.out.println(words.get(i));
+		}
+		double totalCount=0;
+		String longest=words.get(0);
+		for (int i=0; i<words.size(); i++)
+		{
+			if (words.get(i).length() > longest.length())
+				longest = words.get(i);
+			totalCount = totalCount + words.get(i).length();
+		}
+		System.out.println("Avg word length: " +  totalCount/words.size());
+		System.out.println("Longest word: " +  longest);
 
 	}
 
